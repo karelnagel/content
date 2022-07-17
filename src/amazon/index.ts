cb8b219f24c215149e8f6094f8ed5b9ad4320626
Karel <karel789@hotmail.com>
2022-07-17 17:26:48 +0300

    test

--- src/amazon/index.ts
+++ src/amazon/index.ts
@@ -45,7 +45,8 @@ export async function getDuration(text: string) {
     try {
       duration = await getAudioDurationInSeconds(text)
     }
-    catch {
+    catch (e) {
+      console.log(e)
       await new Promise(resolve => setTimeout(resolve, 5000))
     }
   }
